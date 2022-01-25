75b314d4793d4448af1ef63d17fb5924ab4b33a4
Desislava Dimitrova <desislava.dimitrova.it@gmail.com>
2022-01-25 20:05:13 +0200

    Added Move... functions

--- StartUp/SnakeSetUp.h
+++ StartUp/SnakeSetUp.h
@@ -27,11 +27,87 @@ int** ResizeSnake(int** snake, int& length)
 	return newSnake;
 }
 
-void ShowSnake(int** snake, int& length)
+void ShowSnake(int** snake, int length)
 {
-	for (int i = 0; i < length; i++)
+	for (int i = length - 1; i >= 0; i--)
 	{
-		SetCursor(snake[i][0], snake[i][1]);
+		int x = snake[i][0];
+		int y = snake[i][1];
+		SetCursor(x, y);
 		cout << '$';
 	}
+}
+
+void MoveRight(int** snake, int length)
+{
+	int x = snake[length - 1][0];
+	int y = snake[length - 1][1];
+	SetCursor(x, y);
+	cout << ' ';
+	int newX = snake[0][0] + 1;
+	for (int i = length - 1; i > 0; i--)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			snake[i][j] = snake[i - 1][j];
+		}
+	}
+
+	snake[0][0] = newX;
+}
+
+void MoveDown(int** snake, int length)
+{
+	int x = snake[length - 1][0];
+	int y = snake[length - 1][1];
+	SetCursor(x, y);
+	cout << ' ';
+	int newY = snake[0][1] + 1;
+	for (int i = length - 1; i > 0; i--)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			snake[i][j] = snake[i - 1][j];
+		}
+	}
+
+	snake[0][1] = newY;
+}
+
+void MoveLeft(int** snake, int length)
+{
+	int x = snake[length - 1][0];
+	int y = snake[length - 1][1];
+	SetCursor(x, y);
+	cout << ' ';
+	int newX = snake[0][0] - 1;
+	for (int i = length - 1; i > 0; i--)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			x = snake[i - 1][0];
+			y = snake[i - 1][1];
+			snake[i][j] = snake[i - 1][j];
+		}
+	}
+
+	snake[0][0] = newX;
+}
+
+void MoveUp(int** snake, int length)
+{
+	int x = snake[length - 1][0];
+	int y = snake[length - 1][1];
+	SetCursor(x, y);
+	cout << ' ';
+	int newY = snake[0][1] - 1;
+	for (int i = length - 1; i > 0; i--)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			snake[i][j] = snake[i - 1][j];
+		}
+	}
+
+	snake[0][1] = newY;
 }
--- StartUp/StartUp.cpp
+++ StartUp/StartUp.cpp
@@ -21,19 +21,59 @@ int main()
 
 	snake[5] = nullptr;
 
-	int direction = 1;
+	int direction = 1; //initial direction is right
 	//1 = right
 	//2 = down
 	//3 = left
 	//4 = up
 
-	for (int i = 0; i < length; i++)
+	for (int i = length - 1, j = 0; i >= 0; i--, j++)
 	{
-		snake[i][0] = i + 1;
+		snake[i][0] = j + 1;
 		snake[i][1] = 1;
 	}
 
 	ShowSnake(snake, length);
+	int counter = 1;
+
+	while (true)
+	{
+		switch (direction)
+		{
+		case 1:
+			MoveRight(snake, length);
+			break;
+		case 2:
+			MoveDown(snake, length);
+			break;
+		case 3:
+			MoveLeft(snake, length);
+			break;
+		case 4:
+			MoveUp(snake, length);
+			break;
+		}
+		ShowSnake(snake, length);
+		Sleep(500);
+		counter++;
+		if (counter > 10 && counter <= 20)
+		{
+			direction = 2;
+		}
+		if (counter > 20 && counter <= 30)
+		{
+			direction = 3;
+		}
+		if (counter > 30 && counter <= 40)
+		{
+			direction = 4;
+		}
+
+		if (counter > 40)
+		{
+			break;
+		}
+	}
 
 	for (int i = 0; i < length; i++)
 	{
